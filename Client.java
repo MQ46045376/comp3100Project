5a5768918fcc2763e763e55914c97aa1c8dcdbeb
MQ46045376 <100902360+MQ46045376@users.noreply.github.com>
2022-04-07 02:10:47 +1000

    Update Client.java
    This is my commit. Completed up to step 14

--- Client.java
+++ Client.java
@@ -1,42 +1,10 @@
-/*
-1: Create a socket
-2: Initialise input and output streams associated with the socket
-3: Connect ds-server
-4: Send HELO
-5: Receive OK
-6: Send AUTH username
-7: Receive OK
-8: While the last message from ds-server is not NONE do // jobs 1 - n
-9: Send REDY
-10: Receive a message // typically one of the following: JOBN, JCPL and NONE
-//Identify the largest server type; you may do this only once
-11: Send a GETS message, e.g., GETS All
-12: Receive DATA nRecs recSize // e.g., DATA 5 124
-13: Send OK
-14: For i = 0; i < nRecs; ++i do
-15: Receive each record
-16: Keep track of the largest server type and the number of servers of that type
-17: End For
-18: Send OK
-19: Receive .
-20: If the message received at Step 10 is JOBN then
-21: Schedule a job // SCHD
-22: End If
-23: End While
-24: Send QUIT
-25: Receive QUIT
-26: Close the socket
-*/
-
-package ds_sim_Client;
-
 import java.io.*;
 import java.net.*;
 
 public class Client {
 
 	private final static int SocketNumber = 50000; // change this to desired socket number
-	private final static String UserName = "someUserName"; //change this to username
+	private final static String UserName = "someUsername"; //change this to username
 	
 	
 	public static void main(String[] args) {
@@ -44,6 +12,106 @@ public class Client {
 		// LRR = Largest Round Robin
 		try {
 			//1,2,3
+			// Create socket, init in/out streams and connect to ds-server
+			Socket s = new Socket("localhost", SocketNumber);
+			BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+			
+			//4,5
+			dout.write("HELO\n".getBytes()); //send HELO
+			dout.flush(); //Design choice: flushing after every write to ensure nothing messes up in output stream
+			
+			String msg; //msg holds the last msg sent by server
+			msg = in.readLine(); //receive OK
+			System.out.println("DS-Server: " + msg); 
+			
+			//6: send AUTH username
+			dout.write(("AUTH " + UserName + "\n").getBytes());
+			dout.flush();
+
+			// 7: Receive OK
+			msg = in.readLine();
+			System.out.println("DS-Server: " + msg);
+
+			// 8
+			while(!msg.contains("NONE")){
+				//9 send REDY
+				dout.write("REDY\n".getBytes());
+				dout.flush();
+
+				//10 receive JOBN JCPL NONE
+				msg = in.readLine();
+				String jobType = msg; // jobType save type of job i.e. JOBN JCPL or NONE
+				System.out.println("DS-Server: " + msg);
+
+				//find largest server??
+				//String[] serverSortArr = msg.split(" ");
+
+				//11 send GETS ALL
+				dout.write("GETS ALL\n".getBytes());
+				dout.flush();
+
+
+				//12 receive DATA nRec recSize // e.g. DATA 5 124
+				msg = in.readLine();
+				System.out.println("DS-Server: " + msg);
+
+				String[] temp = msg.split(" "); // stores [DATA] [nRec] [recSize] temporarily
+				int nRecs = Integer.parseInt(temp[2]); //   get    ^nRec and turn it into int
+
+				//13 
+				dout.write("OK\n".getBytes()); //send OK
+				dout.flush();
+
+				//14
+				String largestServerType; //stores largest server type
+				int mostCores = -1; //store the largest core size; more cores = bigger server
+				int numberOfLargestServers = 0; //stores number of that server
+
+				/*
+				ASSUMPTION: all server types have unique number of cores; there won't be two different server types with same amount of cores
+				*/
+				for(int i = 0; i < nRecs; i++){
+					//receive each record
+					msg = in.readLine();
+					System.out.println(msg);
+
+					//keep track of largest server type & number of server of that type
+					// split into serverType serverID state curStartTime core memory disk ...
+					temp = msg.split(" ");
+
+					if(Integer.parseInt(temp[4]) > mostCores){ //if this server is bigger than the one on record
+						
+						
+						numberOfLargestServers = 1;
+
+					}
+
+
+
+
+				}
+
+
+
+
+
+
+
+				
+
+			}
+			dout.write(("QUIT\n").getBytes());
+			// recieves quit
+			dout.close();
+			s.close();
+		}
+
+		catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
 			// Create socket, init in/out streams and connect to ds-server
 			Socket s = new Socket("localhost", SocketNumber);
 			BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
